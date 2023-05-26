a629935f21d25edc296c15fef7e0c4c20133c54b
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-05-26 09:24:04 -0700

    a

---
+++ atoicode.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * interactive - function that takes a pointer to a variable
+ * @info: parameter
+ *
+ * Return: an integer value
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - function that checks if the character c
+ * is a delimiter character
+ * @c: character
+ * @delim: pointer to a character
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - check whether the given integer
+ *@c: character
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - function is intended to convert a string
+ * representation of an integer
+ *@s: string
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
